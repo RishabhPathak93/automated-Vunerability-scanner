ca13315c5ba189b65762bb808ee5d7ad10f2a021
Rishabh Pathak <123829611+RishabhPathak93@users.noreply.github.com>
2025-03-26 15:27:26 +0530

    Update urls.py

--- scanner/urls.py
+++ scanner/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ScanView as ScanFolderView, ScanResultsView, ScanProgressView
+from .views import ScanView, ScanResultsView, ScanProgressView
 
 urlpatterns = [
-    path("scan/", ScanFolderView.as_view(), name="scan-folder"),
+    path("scan/", ScanView.as_view(), name="scan-folder"),
     path("results/", ScanResultsView.as_view(), name="scan-results"),
     path("progress/", ScanProgressView.as_view(), name="scan-progress"),
 ]
